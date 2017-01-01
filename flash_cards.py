af28692c23454826de1ebc8ab1d47ced8ade01df
Faraday1221 <faraday1221@gmail.com>
2016-12-31 20:23:45 -0500

    updated to work with Python 2 or 3

--- flash_cards.py
+++ flash_cards.py
@@ -26,6 +26,14 @@ def read_yaml_file(filename):
         yaml_data = f.read()
     return yaml.load(yaml_data)
 
+def prompt_user(input_statement):
+    'selects the appropriate way to prompt the user based on python version'
+    if sys.version_info > (3,0):
+        _ = input(input_statement)
+        return _
+    else:
+        _ = raw_input(input_statement)
+        return _
 
 def ask_questions(all_topics):
     """this will ask the user the questions on the flash cards
@@ -45,11 +53,13 @@ def ask_questions(all_topics):
             # clear screen & ask question
             os.system('clear')
             print('\n'*3,data[topic]['q'])
-            raw_input('\n\n...press any key to see the answer ')
+            prompt_user('\n\n...press any key to see the answer ')
+
 
             # show the answer
             print('\n'*3,data[topic]['a'])
-            status = str(raw_input('\n\nDid you answer correctly? Press "y" or "n"\n')).strip().lower()
+            status = prompt_user('\n\nDid you answer correctly? Press "y" or "n"\n').strip().lower()
+            # status = str(input('\n\nDid you answer correctly? Press "y" or "n"\n')).strip().lower()
 
             # ask if the question should be asked again
             if status == "y":
@@ -70,7 +80,8 @@ def ask_questions(all_topics):
             return
         else:
             print('\n\n\nAll questions have been asked at least once.')
-            kill = str(raw_input('Would you like to stop now? Press "y" or "n"\n')).strip().lower()
+            kill = prompt_user('\nWould you like to stop now? Press "y" or "n"\n').strip().lower()
+            # kill = str(input('Would you like to stop now? Press "y" or "n"\n')).strip().lower()
             if kill == "y":
                 print(status)
                 return
@@ -80,4 +91,4 @@ if __name__ == '__main__':
     # read in the questions from a yaml file and ask questions
     filename_ = sys.argv[1]
     data = read_yaml_file(filename_)
-    ask_questions(data.keys())
+    ask_questions(list(data.keys()))
